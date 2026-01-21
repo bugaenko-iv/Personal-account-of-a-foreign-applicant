d1e5ec3f67b1126558f5b2b13166a62f01105c82
bugaenko-iv <iv.bugaenko@mail.ru>
2026-01-21 12:39:51 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Личный_кабинет_иностранного_абитуриента.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
